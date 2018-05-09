609d4d7efa2a327e7b3f3ee3eb13e956e7efccc5
Apache-HB <35050275+Apache-HB@users.noreply.github.com>
2018-05-09 18:00:39 -0400

    added a wizard for adding games and states

--- presence.py
+++ presence.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 try:
     from pypresence import Presence
@@ -11,6 +12,12 @@ from time import sleep, time
 from datetime import datetime
 import json
 import tkinter
+
+try:
+    if sys.argv[1] == '-w':
+        wizard = True
+except IndexError:
+    wizard = False
 
 default = [
     {
@@ -57,8 +64,42 @@ default = [
 if not os.path.exists('config.json'):
     file = open('config.json', 'w').close()
     json.dump(default, open('config.json', 'w'), indent=4)
+    wizard = True
+    print('Config file was generated')
 
 default = json.load(open('config.json'))
+
+if wizard:
+    next_ = input('Do you want to add a game or a state to a game?').lower()
+    if next_ == 'state':
+        add = input('which game would you like to add this too?')
+
+        another_state = {
+            'name': input('Add name'),
+            'details': input('Input details'),
+            'state': input('input state'),
+            'small_image': input('small image key'),
+            'small_text': input('small image text'),
+            'large_image': input('large image key'),
+            'large_text': input('large image text')
+        }
+        for x in default:
+            if x['name'].lower() == add.lower():
+                x['states'].append(another_state)
+
+    elif next_ == 'game':
+        another_game = {
+            'name': input('new name'),
+            'id': input('input client id'),
+            'states': [
+                {
+
+                }
+            ]
+        }
+
+    else:
+        print('either input game or state')
 
 window = tkinter.Tk()
 window.title('Don\'t close me')
@@ -98,5 +139,6 @@ print(client.update(
     large_text=default[index]['states'][subindex].get('large_text'),
     start=int(time())
 ))
+print('opened a window, just minimize it')
 
 window.mainloop()
